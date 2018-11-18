adbbc08a63c88317ecd8806e4b7fd85886b2b219
fostrb <fostrb@gmail.com>
2018-11-18 12:21:16 -0500

    small updates

--- aliases.zsh
+++ aliases.zsh
@@ -3,4 +3,4 @@ alias ls='ls --color=auto --group-directories-first'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias grin='grep -riIn'
 
-alias pysh='scl enable rh-python36 zsh'
+#alias pysh='scl enable rh-python36 zsh'
